ad92c57525d343f3f854047eb6db06ffdc89e5b9
nitin272 <nitinson95092@gmail.com>
2024-01-31 16:21:57 +0530

    crud operation added

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express")
-
+const route = require('./route')
 const app = express()
 const mongoose = require('mongoose')
-const data ='mongodb+srv://nitinsoni:<Nitin11>@most-searched-web.cc9wsj9.mongodb.net/?retryWrites=true&w=majority'
+const data ='mongodb+srv://nitinsoni:Nitin11@most-searched-web.cc9wsj9.mongodb.net/?retryWrites=true&w=majority'
+app.use('/',route)
 
-app.get("/",(req,res)=>{
+app.get("/DataBAse", (req, res) => {
     mongoose.connect(data)
-    .then(()=>{
-        res.json({Connection_Status: "Connected"})
-    .catch((error)=>{
-        res.json({Connection_Status: "Not Connected"})
+    .then(() => {
+        res.json({ Connection_Status: "Connected" });
     })
-})
-})
+    .catch((error) => {
+        res.json({ Connection_Status: "Not Connected", error: error.message });
+    });
+});
+
 
 app.get("/ping",(req,res)=> {
     res.json({message:'Pong'})
 })
 
-app.listen(3000,() => {
+app.listen(4000,() => {
     console.log("Server is working on port 3000")
 });
