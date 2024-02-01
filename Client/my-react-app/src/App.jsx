96c689dfd0cb0bbcbd8272963fe8d514dbe76edb
nitin272 <nitinson95092@gmail.com>
2024-02-01 21:13:52 +0530

    distt

--- Client/my-react-app/src/App.jsx
+++ Client/my-react-app/src/App.jsx
@@ -6,7 +6,15 @@ function App() {
     <div className="App">
       <div className="content">
         <h1>Most Searched Website on Internet</h1>
-        <button onClick={() => console.log('Go to next page')}>Next</button>
+        <div className="info-container">
+          <div className="image-container">
+            <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_social_share_1024x768_scale,f_auto,q_auto:best/streams/2013/March/130326/1C6639340-google-logo.jpg" alt="Google" />
+          </div>
+          <div className="text-container">
+            <h2>No. 1 Google</h2>
+            <p>Welcome to the world of search engines. Among them, Google stands out as the undisputed leader, serving billions of search queries daily.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
