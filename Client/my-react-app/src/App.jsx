f1711fa316b08b61694b14c610f1d806029b5141
nitin272 <nitinson95092@gmail.com>
2024-02-02 11:59:25 +0530

    component is Added

--- Client/my-react-app/src/App.jsx
+++ Client/my-react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Data from "../Data.json"
 import './App.css';
 
 function App() {
@@ -8,11 +9,16 @@ function App() {
         <h1>Most Searched Website on Internet</h1>
         <div className="info-container">
           <div className="image-container">
-            <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_social_share_1024x768_scale,f_auto,q_auto:best/streams/2013/March/130326/1C6639340-google-logo.jpg" alt="Google" />
+            <img src={Data[0].image} alt="Google" />
           </div>
           <div className="text-container">
-            <h2>No. 1 Google</h2>
-            <p>Welcome to the world of search engines. Among them, Google stands out as the undisputed leader, serving billions of search queries daily.</p>
+            <h2>{Data[0].Website}</h2>
+            <p>{Data[0].Description}</p>
+            <strong>Here is the link </strong>
+            <h2>{Data[0].Link}</h2>
+            <strong>Year</strong>
+            <h2>{Data[0].year}</h2>
+          
           </div>
         </div>
       </div>
